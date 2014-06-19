be20312aae55a004db1b5fa4aa94ef204e259135
Valerio Proietti <kamicane@gmail.com>
2014-06-19 20:27:33 +0200

    implicit superClass (Object), use getPrototypeOf, extend null

--- transform/classes.js
+++ transform/classes.js
@@ -5,8 +5,8 @@ var syntax = require('nodes/syntax.json');
 
 var { applyContext } = require('./spread');
 
-var { insertAfter, insertBefore } = require('../util/insertion');
-var { upper, express } = require('../util/string');
+var { insertAfter } = require('../util/insertion');
+var { express } = require('../util/string');
 var { getUniqueId } = require('../util/id');
 var { getExtendId } = require('../util/extend');
 var { getSelfId } = require('../util/self');
@@ -21,85 +21,75 @@ function classify(program) {
       node.id = node.parentNode.id.clone();
     }
 
-    var name = node.id ? node.id.name : 'Class';
+    if (!node.id) node.id = getUniqueId(node, 'Class');
 
-    var scope = node.scope();
+    var name = node.id.name;
+
+    var getPrototypeOfNamePrototype = `Object.getPrototypeOf(${name}.prototype)`;
+    var getPrototypeOfName = `Object.getPrototypeOf(${name})`;
+
     var extendId = getExtendId(program).clone();
     var superClass = node.superClass;
-
-    var superClassDeclaration;
-
-    if (superClass && superClass.type !== syntax.Identifier) {
-      var superClassId = getUniqueId(scope, `Super${upper(name)}`);
-      superClassDeclaration = new nodes.VariableDeclaration({
-        declarations: [ new nodes.VariableDeclarator({ id: superClassId, init: superClass }) ]
-      });
-      superClass = node.superClass = superClassId.clone();
-    }
 
     var constructorMethod = !!definitions.search('> #MethodDefinition > key[name=constructor]').length;
 
     if (!constructorMethod) definitions.unshift(new nodes.MethodDefinition({
       key: new nodes.Identifier({ name: 'constructor' }),
-      value: superClass ?
-        express(`(function () { ${superClass.name}.apply(this, arguments); })`).expression :
-        express('(function () {})').expression
+      value: express(`(function () {
+        var proto = ${getPrototypeOfNamePrototype};
+        if (proto !== null) proto.constructor.apply(this, arguments);
+      })`).expression
     }));
 
-    if (superClass) {
+    // Super Calls
 
-      // Super Calls
+    var q = [
+      '>> #CallExpression > callee#MemberExpression[computed=false] > object#Identifier[name=super]',
+      '>> #CallExpression > callee#Identifier[name=super]'
+    ];
 
-      var q = [
-        '>> #CallExpression > callee#MemberExpression[computed=false] > object#Identifier[name=super]',
-        '>> #CallExpression > callee#Identifier[name=super]'
-      ];
+    definitions.search(q).forEach((id) => {
+      var definition = id.parent('#MethodDefinition');
 
-      definitions.search(q).forEach((id) => {
-        var definition = id.parent('#MethodDefinition');
+      var parentNode = id.parentNode;
 
-        var parentNode = id.parentNode;
+      var callExpression, methodName;
 
-        var callExpression, methodName;
+      if (parentNode.type === syntax.MemberExpression) {
+        callExpression = parentNode.parentNode;
+        methodName = parentNode.property.name;
+      } else {
+        callExpression = parentNode;
+        methodName = definition.key.name;
+      }
 
-        if (parentNode.type === syntax.MemberExpression) {
-          callExpression = parentNode.parentNode;
-          methodName = parentNode.property.name;
-        } else {
-          callExpression = parentNode;
-          methodName = definition.key.name;
-        }
+      var superMethodXp = definition.static ?
+        express(`${getPrototypeOfName}.${methodName}`).expression :
+        express(`${getPrototypeOfNamePrototype}.${methodName}`).expression;
 
-        var superMethodXp;
+      var selfId;
 
-        if (methodName === 'constructor') {
-          superMethodXp = superClass.clone();
-        } else {
-          superMethodXp = definition.static ?
-            express(`${superClass.name}.${methodName}`).expression :
-            express(`${superClass.name}.prototype.${methodName}`).expression;
-        }
+      // if (!definition.static) {
+        var definitionFunction = definition.value;
 
-        var selfId;
+        selfId = (id.scope() !== definitionFunction) ?
+          getSelfId(definitionFunction).clone() :
+          new nodes.ThisExpression;
+      // }
 
-        if (!definition.static) {
-          var definitionFunction = definition.value;
-
-          selfId = (id.scope() !== definitionFunction) ?
-            getSelfId(definitionFunction).clone() :
-            new nodes.ThisExpression;
-        }
-
-        callExpression.callee = superMethodXp;
-        applyContext(callExpression, selfId);
-      });
-
-    }
+      callExpression.callee = superMethodXp;
+      applyContext(callExpression, selfId);
+    });
 
     var constructorFunction = definitions.search('> #MethodDefinition > key[name=constructor] < * > value')[0];
+    constructorFunction.id = new nodes.Identifier({ name: name });
+
+
     definitions.removeChild(constructorFunction.parentNode);
 
-    if (!superClass) superClass = new nodes.Identifier({ name: 'null' });
+    constructorFunction = new nodes.FunctionDeclaration(constructorFunction);
+
+    if (!superClass) superClass = new nodes.Identifier({ name: 'Object' });
 
     var prototype = new nodes.ObjectExpression;
     var members = new nodes.ObjectExpression;
@@ -114,43 +104,27 @@ function classify(program) {
 
     var extendExpression = express(`${extendId.name}()`);
     var args = extendExpression.expression.arguments;
-    args.push(superClass);
+
+    args.push(superClass, constructorFunction.id.clone(), prototype);
+    if (members.properties.length) args.push(members);
 
     if (node.type === syntax.ClassExpression) {
 
-      // todo: figure out a nice name based on variable name.
-      if (!constructorFunction.id) constructorFunction.id = new nodes.Identifier({ name: name });
+      var wrapper = express('(function(){})()').expression;
+      var body = wrapper.callee.body.body;
+      var returnStatement = new nodes.ReturnStatement({
+        argument: extendExpression.expression
+      });
 
-      args.push(constructorFunction);
+      body.push(constructorFunction);
+      body.push(returnStatement);
 
-      if (superClassDeclaration) {
-        var wrapper = express('(function(){})()').expression;
-        var body = wrapper.callee.body.body;
-        var returnStatement = new nodes.ReturnStatement({
-          argument: extendExpression.expression
-        });
-
-        if (superClassDeclaration) body.push(superClassDeclaration);
-        body.push(returnStatement);
-
-        node.parentNode.replaceChild(node, wrapper);
-
-      } else {
-        node.parentNode.replaceChild(node, extendExpression.expression);
-      }
+      node.parentNode.replaceChild(node, wrapper);
 
     } else {
-
-      constructorFunction = new nodes.FunctionDeclaration(constructorFunction);
-      constructorFunction.id = node.id;
-      args.push(constructorFunction.id.clone());
       node.parentNode.replaceChild(node, constructorFunction);
       insertAfter(constructorFunction, extendExpression);
-      if (superClassDeclaration) insertBefore(constructorFunction, superClassDeclaration);
     }
-
-    args.push(prototype);
-    if (members.properties.length) args.push(members);
 
   });
 
